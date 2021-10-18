096d9fea0f1330c32f2a4c08f15fd4a32a53c891
bojanabb <bojana.buljic@gmail.com>
2021-10-18 18:07:39 +0200

    update index.html - add default  modal

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,16 @@
 const hamburgerEl = document.querySelector(".js-humburger");
 const navigationLinks = document.querySelector(".js-navlinks");
 const closeBtn = document.querySelector(".js-close");
+const page = document.querySelector(".js-page");
 
 hamburgerEl.addEventListener('click', () => {
-    hamburgerEl.classList.add("hide")
-    navigationLinks.classList.add('show')
+    hamburgerEl.classList.add("hide");
+    navigationLinks.classList.add('show');
+    page.classList.add('nav-shown');
 });
 
 closeBtn.addEventListener('click', () => {
     navigationLinks.classList.remove('show');
     hamburgerEl.classList.remove('hide');
+    page.classList.remove('nav-shown');
 });
