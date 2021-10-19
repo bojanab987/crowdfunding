9c67714161c71a0b48365165687962fdef43860e
bojanabb <bojana.buljic@gmail.com>
2021-10-19 17:58:34 +0200

    add overlay when navigation is open on smaller screens

---
+++ js/modals.js
@@ -0,0 +1,8 @@
+const modalEl = document.querySelector('.js-modal-default');
+const pageEl = document.querySelector('.js-page');
+
+// if (!modalEl.hidden) {
+//     pageEl.classList.add('modal-open-shadow')
+// } else {
+//     pageEl.classList.remove('modal-open-shadow')
+// }
--- js/navbar.js
+++ js/navbar.js
@@ -2,15 +2,18 @@ const hamburgerEl = document.querySelector(".js-humburger");
 const navigationLinks = document.querySelector(".js-navlinks");
 const closeBtn = document.querySelector(".js-close");
 const page = document.querySelector(".js-page");
+const overlay = document.querySelector('.modal-overlay')
 
 hamburgerEl.addEventListener('click', () => {
     hamburgerEl.classList.add("hide");
     navigationLinks.classList.add('show');
-    page.classList.add('nav-shown');
+    // page.classList.add('nav-shown');
+    overlay.classList.add('nav-shown');
 });
 
 closeBtn.addEventListener('click', () => {
     navigationLinks.classList.remove('show');
     hamburgerEl.classList.remove('hide');
-    page.classList.remove('nav-shown');
+    overlay.classList.remove('nav-shown');
+    // page.classList.remove('nav-shown');
 });
